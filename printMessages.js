256a0ed9b3e3b6e39fa4b52e4cf3afcd9616f555
Michael <dohertym12@hotmail.com>
2020-06-20 12:45:42 +0800

    finally can find the title in each link - now need to find the link titled Bitcoin

--- printMessages.js
+++ printMessages.js
@@ -13,18 +13,47 @@ request({
 
   console.log(title.text());
 
-  let allElts = $('*');
+  const links = [];
 
-  let filteredElts = allElts.filter(function(i, elt) {
-    // this === elt
-    return $(this).children().length > 3; 
+  let allLinks = $('a');
+
+  allLinks.each((index, element) => {
+    links.push($(element));
   });
 
-  let items = filteredElts.get();
 
-  items.forEach(e => {
-    console.log(e.name);
+  links.forEach(e => {
+    console.log("title is " + e.attr('title'));
+    console.log("name is " + e.attr('name'));
+    console.log("href is " + e.attr('href'));
   });
+
+// this works!!
+//  $('a').each( (index, singleLink) => {
+//    var singleHref = $(singleLink).attr('href');
+//    console.log(singleHref);
+//  });
+
+//  for ( var prop in links[0] ) {
+//    console.log(prop); 
+//  }
+
+//  links.forEach(e => {
+//    for (var prop in e) {
+//      console.log(prop);
+//    }
+//  });
+
+//  let filteredElts = allElts.filter(function(i, elt) {
+//    // this === elt
+//    return $(this).children().length > 3; 
+//  });
+
+//  let items = filteredElts.get();
+
+//  items.forEach(e => {
+//    console.log(e.name);
+//  });
 
 });
 
