2ef415c252312bdb14d61aec35c67d0f22a3ca98
Michael <dohertym12@hotmail.com>
2020-06-20 11:04:12 +0800

    first successful http request response.

--- printMessages.js
+++ printMessages.js
@@ -1,4 +1,18 @@
 let request = require('request');
+let cheerio = require('cheerio');
+
+request({
+  method: 'GET',
+  url: 'https://cryptowat.ch/'
+}, (err, res, body) => {
+  if (err) return console.error(err);
+
+let $ = cheerio.load(body);
+
+let title = $('title');
+
+console.log(title.text());
+});
 
 const messageModule = require('./messages');
 messageModule.messages.forEach( (m)=>  {
